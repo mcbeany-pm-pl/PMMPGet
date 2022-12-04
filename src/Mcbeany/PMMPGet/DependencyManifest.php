5eb57f60f33d03c3e9bbbcedc2d40177d61917e3
Mcbeany <mcbeanyoof@gmail.com>
2022-12-04 14:03:53 +0700

    Indent using 4 spaces

--- src/Mcbeany/PMMPGet/DependencyManifest.php
+++ src/Mcbeany/PMMPGet/DependencyManifest.php
@@ -5,32 +5,32 @@ declare(strict_types=1);
 namespace Mcbeany\PMMPGet;
 
 class DependencyManifest{
-  public function __construct(
-    private PMMPGet $main,
-    private int $id,
-    private string $name,
-    private string $version,
-    private bool $required
-  ){
-  }
+    public function __construct(
+        private PMMPGet $main,
+        private int $id,
+        private string $name,
+        private string $version,
+        private bool $required
+    ){
+    }
 
-  public function getId(): int{
-    return $this->id;
-  }
+    public function getId(): int{
+        return $this->id;
+    }
 
-  public function getName(): string{
-    return $this->name;
-  }
+    public function getName(): string{
+        return $this->name;
+    }
 
-  public function getVersion(): string{
-    return $this->version;
-  }
+    public function getVersion(): string{
+        return $this->version;
+    }
 
-  public function isRequired(): bool{
-    return $this->required;
-  }
+    public function isRequired(): bool{
+        return $this->required;
+    }
 
-  public function asPlugin(): PluginManifest{
-    return $this->main->getCachedPlugin($this->id);
-  }
+    public function asPlugin(): PluginManifest{
+        return $this->main->getCachedPlugin($this->id);
+    }
 }
