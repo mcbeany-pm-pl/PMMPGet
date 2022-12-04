fee1b7e7765986b3c1b98bb2732f391468d43a00
Mcbeany <mcbeanyoof@gmail.com>
2022-12-04 14:06:07 +0700

    Format all codes

--- src/Mcbeany/PMMPGet/DependencyManifest.php
+++ src/Mcbeany/PMMPGet/DependencyManifest.php
@@ -4,33 +4,39 @@ declare(strict_types=1);
 
 namespace Mcbeany\PMMPGet;
 
-class DependencyManifest{
+class DependencyManifest
+{
     public function __construct(
         private PMMPGet $main,
         private int $id,
         private string $name,
         private string $version,
         private bool $required
-    ){
+    ) {
     }
 
-    public function getId(): int{
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function getName(): string{
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function getVersion(): string{
+    public function getVersion(): string
+    {
         return $this->version;
     }
 
-    public function isRequired(): bool{
+    public function isRequired(): bool
+    {
         return $this->required;
     }
 
-    public function asPlugin(): PluginManifest{
+    public function asPlugin(): PluginManifest
+    {
         return $this->main->getCachedPlugin($this->id);
     }
 }
--- src/Mcbeany/PMMPGet/PluginManifest.php
+++ src/Mcbeany/PMMPGet/PluginManifest.php
@@ -7,7 +7,8 @@ namespace Mcbeany\PMMPGet;
 use pocketmine\plugin\ApiVersion;
 use pocketmine\utils\VersionString;
 
-class PluginManifest{
+class PluginManifest
+{
     /**
      * @param string[] $keywords
      * @param string[] $apis
@@ -25,67 +26,78 @@ class PluginManifest{
         private array $keywords,
         private array $apis,
         private array $dependencies
-    ){
+    ) {
     }
 
-    public function getId(): int{
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function getName(): string{
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function getRepoName(): string{
+    public function getRepoName(): string
+    {
         return $this->repoName;
     }
 
-    public function getVersion(): string{
+    public function getVersion(): string
+    {
         return $this->version;
     }
 
-    public function getContentUrl(): string{
+    public function getContentUrl(): string
+    {
         return $this->contentUrl;
     }
 
-    public function getDescription(): string{
+    public function getDescription(): string
+    {
         return $this->description;
     }
 
-    public function getLastUpdated(): int{
+    public function getLastUpdated(): int
+    {
         return $this->lastUpdated;
     }
 
     /**
      * @return string[]
      */
-    public function getKeywords(): array{
+    public function getKeywords(): array
+    {
         return $this->keywords;
     }
 
     /**
      * @return string[]
      */
-    public function getApis(): array{
+    public function getApis(): array
+    {
         return $this->apis;
     }
 
     /**
      * @return DependencyManifest[]
      */
-    public function getDependencies(): array{
+    public function getDependencies(): array
+    {
         return $this->dependencies;
     }
 
-    public function isCompatible(bool $withDependencies = false): bool{
-        if($withDependencies){
-        foreach($this->dependencies as $dependency){
-            $version = new VersionString($dependency->getVersion());
-            $plugin = $this->main->getServer()->getPluginManager()->getPlugin($dependency->getName());
-            if(($plugin == null && $dependency->isRequired()) || $version->compare(new VersionString($plugin->getDescription()->getVersion()), true) > 0){
-            return false;
+    public function isCompatible(bool $withDependencies = false): bool
+    {
+        if ($withDependencies) {
+            foreach ($this->dependencies as $dependency) {
+                $version = new VersionString($dependency->getVersion());
+                $plugin = $this->main->getServer()->getPluginManager()->getPlugin($dependency->getName());
+                if (($plugin == null && $dependency->isRequired()) || $version->compare(new VersionString($plugin->getDescription()->getVersion()), true) > 0) {
+                    return false;
+                }
             }
-        }
         }
         return ApiVersion::isCompatible($this->main->getServer()->getApiVersion(), $this->apis);
     }
